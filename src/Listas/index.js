792042cd946a407dbdc714b9caec2f83d08e805e
Nicolás Gonçalves Batista da Silva <nicolasgbdas@gmail.com>
2021-04-29 11:48:19 -0300

    adicionado lista no projeto react

---
+++ src/Listas/index.js
@@ -0,0 +1,17 @@
+function Lista(){
+    return <div>
+                <ul>
+                    <li>
+                        Carlos
+                    </li>
+                    <li>
+                        João
+                    </li>
+                    <li>
+                        Lucas
+                    </li>
+                </ul>
+            </div>
+    
+}
+export default Lista
